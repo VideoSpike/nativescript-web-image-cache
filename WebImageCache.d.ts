d4e99ae1a93966c7aaca5d3d008440874fb768cc
Morten Sjøgren <m_abs@mabs.dk>
2017-01-04 19:29:59 +0100

    chor: update WebImageCache.d.ts

--- WebImageCache.d.ts
+++ WebImageCache.d.ts
@@ -2,6 +2,38 @@
 import { View } from 'ui/core/view';
 
 export class WebImage extends View {
+  /**
+   * Image src, either res://, http://, ~/... or absolute path
+   */
   src: string;
+
+  /**
+   * Is the image loading or not?
+   */
   isLoading: boolean;
+
+  /**
+   * aspectFit, aspectFill, fill or none
+   */
+  stretch: string;
+
+  /**
+   * Native android object
+   */
+  android: any /* com.facebook.drawee.view.SimpleDraweeView */;
+
+  /**
+   * Native ios object
+   */
+  ios: any /* UIImageView */;
 }
+
+/**
+ * Clear cached images
+ */
+export function clearCache(): void;
+
+/**
+ * For android only: initialize to use cache
+ */
+export function initialize(): void;
