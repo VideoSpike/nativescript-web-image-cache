231f928fb7fd6729cc771ec90cc28d49df2096cc
Morten Sjøgren <m_abs@mabs.dk>
2017-01-04 18:33:18 +0100

    fix: #12 bundling with nativescript-dev-webpack

---
+++ WebImageCache.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="node_modules/tns-core-modules/tns-core-modules.d.ts" />
+import { View } from 'ui/core/view';
+
+export class WebImage extends View {
+  src: string;
+  isLoading: boolean;
+}
